3ea8da7fbc590280b90c1ef6844fcae2dabcfef3
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2012-01-26 13:14:56 -0600

    [IronPythonBinding] Improve the indentation detection.

--- extras/IronPythonBinding/IronPythonBinding/MonoDevelop.IronPython.Formatting/PythonEditorIndentation.cs
+++ extras/IronPythonBinding/IronPythonBinding/MonoDevelop.IronPython.Formatting/PythonEditorIndentation.cs
@@ -1,6 +1,7 @@
 // PythonEditorIndentation.cs
 //
 // Copyright (c) 2008 Christian Hergert <chris@dronelabs.com>
+//               2012 Carlos Alberto Cortez <calberto.cortez@gmail.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -36,19 +37,24 @@ namespace MonoDevelop.IronPython.Formatting
 {
 	public class PythonEditorIndentation : TextEditorExtension
 	{
+		const string StdPythonIndentation = "    "; // 4 spaces
+		
 		public override bool KeyPress (Gdk.Key key, char keyChar, Gdk.ModifierType modifier)
 		{
-			if (key == Gdk.Key.Return)
-			{
-				string lastLine = Editor.GetLineText (Editor.Caret.Line);
-				string trimmed = lastLine.Trim ();
+			if (Editor.IsSomethingSelected)
+				return base.KeyPress (key, keyChar, modifier);
+			
+			if (key == Gdk.Key.Return || key == Gdk.Key.KP_Enter) {
+				string line = GetCurrentLine ();
+				string trimmed = line.Trim ();
 				bool indent = false;
+				bool removeIndent = false;
 				
 				if ((modifier & Gdk.ModifierType.ControlMask) != 0)
 				{
 					Editor.Insert (Editor.Caret.Offset, "\n");
 
-					string endTrim = lastLine.TrimEnd ();
+					string endTrim = line.TrimEnd ();
 					if (!String.IsNullOrEmpty (endTrim))
 					{
 						int i = 0;
@@ -63,38 +69,92 @@ namespace MonoDevelop.IronPython.Formatting
 						
 						return false;
 					}
-					else if (lastLine.Length > 4)
+					else if (line.Length > 4)
 					{
 						// get the last line, remove 4 chars from it if we can
-						Editor.Insert (Editor.Caret.Offset, lastLine.Substring (0, lastLine.Length - 4));
+						Editor.Insert (Editor.Caret.Offset, line.Substring (0, line.Length - 4));
 						return false;
 					}
 				}
-
+				
 				if (trimmed.EndsWith (":"))
 				{
-					indent = true;
+					// Only indent if we are at the very end.
+					if (Editor.Caret.Column - 1 >= line.Length)
+						indent = true;
 				}
 				else if (trimmed.StartsWith ("if ") ||
 				         trimmed.StartsWith ("def "))
 				{
-					int openCount = lastLine.Split ('(').Length;
-					int closeCount = lastLine.Split (')').Length;
+					int openCount = line.Split ('(').Length;
+					int closeCount = line.Split (')').Length;
 
 					if (openCount > closeCount)
 						indent = true;
-				}
+				} else if (trimmed.StartsWith ("return") ||
+						trimmed.Equals ("pass"))
+					removeIndent = true;
 
 				if (indent)
 				{
-					base.KeyPress (key, keyChar, modifier);
-					Editor.Insert (Editor.Caret.Offset, "    ");
-					Editor.Caret.Offset += 4;
+					bool retval = base.KeyPress (key, keyChar, modifier);
+					InsertIndentation ();
+					return retval;
+				} else if (removeIndent) {
+					bool retval = base.KeyPress (key, keyChar, modifier);
+					RemoveIndentation ();
+					return retval;
+				}
+			}
+			
+			if (key == Gdk.Key.Tab) {
+				string line = GetCurrentLine ();
+				
+				if (line.Trim ().Length == 0) {
+					InsertIndentation ();
 					return false;
 				}
 			}
 			
+			if (key == Gdk.Key.BackSpace) {
+				if (RemoveIndentation ())
+					return false;
+			}
+			
 			return base.KeyPress (key, keyChar, modifier);
+		}
+		
+		string GetCurrentLine ()
+		{
+			return Editor.GetLineText (Editor.Caret.Line);
+		}
+		
+		void InsertIndentation ()
+		{
+			Editor.Insert (Editor.Caret.Offset, StdPythonIndentation);
+			Editor.Caret.Offset += StdPythonIndentation.Length;
+			Editor.Document.CommitLineUpdate (Editor.Caret.Line);
+		}
+		
+		bool RemoveIndentation ()
+		{
+			int indentLength = StdPythonIndentation.Length;
+			int column = Editor.Caret.Column - 1; // column is 1-based
+			
+			if (column < indentLength)
+				return false;
+			
+			string line = GetCurrentLine ();
+			if (String.Compare (line, line.Length - indentLength, 
+					StdPythonIndentation, 0, indentLength) != 0)
+				return false;
+			
+			int prevOffset = Editor.Caret.Offset;
+			Editor.Caret.Offset -= indentLength;
+			Editor.Remove (prevOffset - indentLength, indentLength);
+			Editor.Document.CommitLineUpdate (Editor.Caret.Line);
+			
+			return true;
 		}
 	}
 }
